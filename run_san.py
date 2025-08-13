fixing spatial frequency scaling

run_san.py
@@ -3,6 +3,7 @@ from models import KeckTelescope
 from san import SpeckleAreaNulling
 from influenc_funcs import gaussian_influence_function
 import ipdb
+import numpy as np
 
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
@@ -33,7 +34,6 @@ plt.figure()
 plt.title("Dark Frame")
 plt.imshow(keck.get_dark_image(), cmap="gray")
 plt.colorbar()
-plt.show()
 
 # Here for normalization, not strictly necessary
 ref = 1
@@ -51,7 +51,6 @@ plt.subplot(133)
 plt.title("Coronagraphic Image H-band")
 plt.imshow(keck.get_coronagraph_image(include_fpm=True, wfe=wavefront_error) / ref, cmap="inferno", norm=LogNorm())
 plt.colorbar(label="Normalized Intensity")
-plt.show()
 
 
 # Set up a deformable mirror
@@ -78,17 +77,38 @@ san = SpeckleAreaNulling(
     dm=dm,
     IWA=4,
     OWA=7,
-    angular_range=[85, -85],
+    angular_range=[-85, 85],
 )
 
-ipdb.set_trace()
+plt.figure()
+plt.subplot(121)
+plt.title("Sin Probe")
+plt.imshow(san.sin_probe, cmap="RdBu_r")
+plt.subplot(122)
+plt.title("Cos Probe")
+plt.imshow(san.cos_probe, cmap="RdBu_r")
 
-NSTEPS = 4
+NSTEPS = 5
 corrected_images = []
+mean_in_dh = []
 for i in range(NSTEPS):
-    img = san.step(regularization=5e4)
+    img = san.step(regularization=100000)
     corrected_images.append(img)
-    plt.figure()
-    plt.imshow(img, cmap="inferno", norm=LogNorm())
-    plt.colorbar()
-    plt.show()
+    mean_in_dh.append(np.mean(img[san.dh==1]))
+    plt.figure(figsize=[12, 4])
+    plt.suptitle(f"Iteration {i+1}")
+    plt.subplot(131)
+    plt.imshow(san.Im1, cmap="coolwarm")
+    plt.contour(san.dh, levels=[0,5], colors="w")
+    plt.subplot(132)
+    plt.imshow(san.Im2, cmap="coolwarm")
+    plt.contour(san.dh, levels=[0,5], colors="w")
+    plt.subplot(133)
+    plt.imshow(img, norm=LogNorm())
+    plt.contour(san.dh, levels=[0,5], colors="w")
+
+plt.figure()
+plt.plot(mean_in_dh, marker="o")
+plt.ylabel("Intensity, arb. units")
+plt.title("Mean in Dark Hole v.s. Iteration")
+plt.show()

san.py
@@ -104,14 +104,15 @@ class SpeckleAreaNulling:
 
         # Make the DM shapes
         xdm, ydm = make_xy_grid(self.nact, dx=self.dm.act_pitch)
-        arg = xfreq[..., None, None] * xdm + yfreq[..., None, None] * ydm
-        self.cos_modes = np.cos(arg)
-        self.sin_modes = np.sin(arg)
 
         # NOTE This is a spatial frequency fudge factor, something is off
         # by a couple factors of two
-        xdm /= 8
-        ydm /= 8
+        xdm *= 128
+        ydm *= 128
+        
+        arg = xfreq[..., None, None] * xdm + yfreq[..., None, None] * ydm
+        self.cos_modes = np.cos(arg)
+        self.sin_modes = np.sin(arg)
 
         V1sum = np.sum(self.sin_modes, axis=0)
         V2sum = np.sum(self.cos_modes, axis=0)
@@ -120,12 +121,13 @@ class SpeckleAreaNulling:
         V2norm = np.abs(V2sum).max()
         V1sum /= V1norm
         V2sum /= V2norm
+        
+        PROBE_AMP = 1/10
+        self.sin_probe = V1sum * PROBE_AMP 
+        self.cos_probe = V2sum * PROBE_AMP
 
-        self.sin_probe = V1sum / 10
-        self.cos_probe = V2sum / 10
-
-        self.sin_modes /= V1norm * 10
-        self.cos_modes /= V2norm * 10
+        self.sin_modes /= V1norm / PROBE_AMP
+        self.cos_modes /= V2norm / PROBE_AMP
 
     def step(self, regularization=0):
         """Advance the algorithm one iteration
@@ -154,8 +156,8 @@ class SpeckleAreaNulling:
         Im2 = self.images[-2]  # minus cos probe
         Ip2 = self.images[-1]  # plus cos probe
 
-        self.Im1 = Im1
-        self.Im2 = Im2
+        self.Im1 = (Im1 - Ip1)
+        self.Im2 = (Im2 - Ip2)
 
         # Compute the relevant quantities
         dE1 = (Ip1 - Im1) / 4
@@ -178,13 +180,6 @@ class SpeckleAreaNulling:
         correction = np.sum(correction, axis=0)
         self.dm.actuators[:] -= correction
         self.dm_surface.append(self.dm.actuators)
-
-        import matplotlib.pyplot as plt
-
-        plt.figure()
-        plt.imshow(self.dm.render(wfe=True), cmap="RdBu_r")
-        plt.colorbar()
-        plt.show()
 
         # return an image
         img = self.fwd(self.dm.render(wfe=True)) / self.ref_contrast